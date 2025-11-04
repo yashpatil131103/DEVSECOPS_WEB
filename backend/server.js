f009711dd19aa6fdd4e87a84c7803e25b260f727
Pushpak <154728724+Pushpak3504@users.noreply.github.com>
2025-11-04 19:53:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,4 @@ app.get("/api", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on http://3.110.205.36:${PORT}`));
